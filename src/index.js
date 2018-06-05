05c09be8b22575b56d71d03ba6775dcb5c491f5a
Aman Sahil <aman.salam2@gmail.com>
2018-06-06 01:48:06 +0400

    Fixes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import App from './components/App.js';
 import * as firebase from 'firebase';
 import registerServiceWorker from './registerServiceWorker';
 var config = {
-    apiKey: "AIzaSyDWGJ4QNaBeiM5EAF5Onw2TBmhVNmy3KyM",
-    authDomain: "firephoto-b8be7.firebaseapp.com",
-    databaseURL: "https://firephoto-b8be7.firebaseio.com",
-    projectId: "firephoto-b8be7",
-    storageBucket: "firephoto-b8be7.appspot.com",
-    messagingSenderId: "916626634338"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 firebase.initializeApp(config);
 
